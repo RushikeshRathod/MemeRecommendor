19deaab5ec567095c13ae680512ee90839e13d04
rushikesh <rushikeshbrathod@gmail.com>
2018-04-30 21:05:50 +0530

    made changes in template

--- datacollection/datacollection/urls.py
+++ datacollection/datacollection/urls.py
@@ -20,5 +20,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^rushikesh/',include('rushikesh.urls')),
-    url(r'^kartik/',include('kartik.urls')),
+    url(r'^void/',include('kartik.urls')),
 ]
